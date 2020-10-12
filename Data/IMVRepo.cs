824e840248ae0c7cbcb454b5289312a4ff2c39db
Chaz <chazsherrill@icloud.com>
2020-10-12 14:18:57 -0500

    Building out the repo

---
+++ Data/IMVRepo.cs
@@ -0,0 +1,11 @@
+﻿using MVShop2._1.Data.Entities;
+using System.Collections.Generic;
+
+namespace MVShop2._1.Data
+{
+    public interface IMVRepo
+    {
+        IEnumerable<Product> GetAllProducts();
+        IEnumerable<Product> GetProductsByCategory(string category);
+    }
+}
---
+++ Data/MVRepo.cs
@@ -0,0 +1,31 @@
+﻿using MVShop2._1.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVShop2._1.Data
+{
+    public class MVRepo : IMVRepo
+    {
+        private readonly MVContext _ctx;
+
+        public MVRepo(MVContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _ctx.Products
+                        .OrderBy(p => p.Name)
+                        .ToList();
+        }
+
+        public IEnumerable<Product> GetProductsByCategory(string category)
+        {
+            return _ctx.Products
+                        .Where(p => p.Category == category)
+                        .ToList();
+        }
+    }
+}
